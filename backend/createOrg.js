8abfef0d59b0ab264118f18feed6efc1d0ab137f
Karan Bhardwaj <karanbhardwaj0077@gmail.com>
2026-04-04 22:44:13 +0530

    Update createOrg.js

--- backend/createOrg.js
+++ backend/createOrg.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Organisation = require("./models/Organisation");
 
-mongoose.connect("mongodb://127.0.0.1:27017/university_sports")
+mongoose.connect(process.env.MONGO_URI)
 .then(async () => {
 
   await Organisation.create({
