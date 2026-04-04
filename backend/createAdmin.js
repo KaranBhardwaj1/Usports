d63aa3b344ba51bea9c5158efe2f6ff834f1ad28
Karan Bhardwaj <karanbhardwaj0077@gmail.com>
2026-04-04 22:44:31 +0530

    Update createAdmin.js

--- backend/createAdmin.js
+++ backend/createAdmin.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const User = require("./models/User");
 
-mongoose.connect("mongodb://127.0.0.1:27017/university_sports");
+mongoose.connect("process.env.MONGO_URI");
 
 async function createAdmin() {
   const hashedPassword = await bcrypt.hash("admin123", 10);
